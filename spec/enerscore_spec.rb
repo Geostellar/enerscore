275b3caa21d37283989440b402189d90db06500e
jgeo <joseph.tutela@geostellar.com>
2016-05-24 15:30:31 -0700

    remove unused spec

--- spec/enerscore_spec.rb
+++ spec/enerscore_spec.rb
@@ -4,8 +4,4 @@ describe Enerscore do
   it 'has a version number' do
     expect(Enerscore::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
